76d85f06b946877db96220792bf10ac93442264e
bergerch <bergerch@fim.uni-passau.de>
2017-03-22 13:56:11 +0100

    Update project dependencies

--- src/web-bft/src/app/app.component.ts
+++ src/web-bft/src/app/app.component.ts
@@ -8,31 +8,32 @@ import { Subject, Observable, Subscription } from 'rxjs/Rx';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-    
-    private title = 'Web-BFT Demo!';
-    private socket: Subject<any>;
-    private counterSubscription: Subscription;
-    private message: string;
-    private sentMessage: string;
-    
-    constructor(websocketService: WebsocketService) {
-        this.socket = websocketService.createWebsocket();
+
+  private title = 'Web-BFT Demo!';
+  private socket: Subject<any>;
+  private counterSubscription: Subscription;
+  private message: string;
+  private sentMessage: string;
+
+  constructor(websocketService: WebsocketService) {
+    this.socket = websocketService.createWebsocket();
+  }
+
+  ngOnInit() {
+    this.socket.subscribe(message => this.message = message.data);
+  }
+
+  private launchCounter() {
+    // Counter already initialized
+    if (this.counterSubscription) {
+      this.counterSubscription.unsubscribe();
     }
-    
-    ngOnInit() {
-        this.socket.subscribe(message => this.message = message.data);
-    }
-    
-    private launchCounter() { 
-        //Counter already initialized
-        if(this.counterSubscription) {
-            this.counterSubscription.unsubscribe();
-        }
-        
-        let counter = Observable.interval(1000);
-        this.counterSubscription = counter.subscribe(num => {
-            this.sentMessage = 'Websocket Message '+ num;
-            this.socket.next(this.sentMessage);
-        });
-    }
+
+    let counter = Observable.interval(1000);
+    this.counterSubscription = counter.subscribe(num => {
+      this.sentMessage = 'Websocket Message ' + num;
+      this.socket.next(this.sentMessage);
+    });
+  }
 }
+
