8fe20567a1630633ed0b090e34ba4124cd144efa
bergerch <bergerch@fim.uni-passau.de>
2017-04-23 14:48:13 +0200

    Grundgerüst für ServiceProxy

---
+++ src/web-bft/src/bft/tom/ServiceProxy.service.ts
@@ -0,0 +1,71 @@
+/**
+ * Created by chris on 23.04.17.
+ */
+import {Injectable} from '@angular/core';
+
+export enum TOMMessageType {
+  ORDERED_REQUEST = 0,
+  UNORDERED_REQUEST = 1,
+  REPLY = 2,
+  RECONFIG = 3,
+  ASK_STATUS = 4,
+  STATUS_REPLY = 5,
+  UNORDERED_HASHED_REQUEST = 6
+}
+
+export interface TOMMessage {
+  viewID: number;
+  type: TOMMessageType; // request type: application or reconfiguration request
+  session: number; // Sequence number defined by the client
+  // There is a sequence number for ordered and another for unordered messages
+  sequence: number;
+  operationId: number; // Sequence number defined by the client
+  content: any; // Content of the message
+}
+
+@Injectable()
+export class ServiceProxy {
+
+  reqId: number = -1;
+  operationId: number = -1;
+  replyQuorum: number = 0; // size of the reply quorum
+  receivedReplies: number = 0; // Number of received replies
+  invokeTimeout: number = 40;
+  replyServer: number;
+  invokeUnorderedHashedTimeout: number = 10;
+
+  requestType: TOMMessageType;
+  replies: TOMMessage[] = []; // Replies from replicas are stored here
+
+  // Comparator<byte[]> comparator;
+  // Extractor extractor;
+  // Random rand = new Random(System.currentTimeMillis());
+  // HashResponseController hashResponseController;
+  // TOMMessage response = null; // Reply delivered to the application
+
+
+  public ServiceProxy() {
+
+  }
+
+  invokeOrdered(request) {
+    return this.invoke(request, 'TOMMessageType.ORDERED_REQUEST');
+  }
+
+  invokeUnordered(request) {
+    return this.invoke(request, 'TOMMessageType.UNORDERED_REQUEST');
+  }
+
+  invokeUnorderedHashed(request) {
+    return this.invoke(request, 'TOMMessageType.UNORDERED_HASHED_REQUEST');
+  }
+
+  invoke(request, reqType) {
+    // TODO
+  }
+
+  private reconfigureTo(view) {
+    // TODO
+  }
+
+}
---
+++ src/web-bft/src/bft/tom/TOMSender.service.ts
@@ -0,0 +1,84 @@
+/**
+ * Created by chris on 23.04.17.
+ */
+
+import {Injectable} from '@angular/core';
+import {TOMMessage, TOMMessageType} from "./ServiceProxy.service";
+
+
+export interface ReplyReceiver {
+
+  /**
+   * This is the method invoked by the client side comunication system, and where the
+   * code to handle the reply is to be written
+   *
+   * @param reply The reply delivered by the client side comunication system
+   */
+   replyReceived(reply: TOMMessage);
+
+}
+
+export interface Closeable {
+  /**
+   * Closes this stream and releases any system resources associated
+   * with it. If the stream is already closed then invoking this
+   * method has no effect.
+   */
+  close();
+
+}
+
+@Injectable()
+export abstract class TOMSender implements ReplyReceiver, Closeable {
+
+  public TOMSender() {
+
+  }
+
+  replyReceived(reply: TOMMessage) {
+
+  }
+
+  close() {
+
+  };
+
+  init(processId: number, configHome: string) {
+
+  }
+
+  startsCS(clientId: number) {
+
+  }
+
+  generateRequestId(type: TOMMessageType): number {
+    return 0;
+  }
+
+  generateOperationId(): number {
+    return 0;
+  }
+
+
+    /**
+     * Multicast a TOMMessage to the group of replicas
+     *
+     * @param sm Message to be multicast
+     */
+    TOMulticastTOM(sm: TOMMessage) {
+    //cs.send(useSignatures, this.viewController.getCurrentViewProcesses(), sm);
+    }
+
+  /**
+   * Multicast data to the group of replicas
+   *
+   * @param m Data to be multicast
+   * @param reqId unique integer that identifies this request
+   * @param reqType TOM_NORMAL, TOM_READONLY or TOM_RECONFIGURATION
+   */
+   TOMulticastData(m: any, reqId: number, reqType: TOMMessageType) {
+
+  }
+  
+
+}
