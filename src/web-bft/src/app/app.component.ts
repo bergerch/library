94bb7925a7fc51e05be57681a21a751b11b58a00
bergerch <bergerch@fim.uni-passau.de>
2017-05-09 13:56:25 +0200

    Counter app for testing purpose

--- src/web-bft/src/app/app.component.ts
+++ src/web-bft/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, Output, EventEmitter, OnInit} from '@angular/core';
 import {WebsocketService} from './websocket.service';
 import {Subject, Observable, Subscription} from 'rxjs/Rx';
 
@@ -8,34 +8,59 @@ import {Subject, Observable, Subscription} from 'rxjs/Rx';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
+  @Input() count = 0;
+  @Output() countChange = new EventEmitter<number>();
 
-  private title = 'Web-BFT Demo!';
-  private socket: Subject<any>;
-  private counterSubscription: Subscription;
-  private message: string;
-  private sentMessage: string;
-  private counter: number = 0;
+  title = 'Web-BFT Demo!';
+  socket: Subject<any>;
+  counterSubscription: Subscription;
+  message: string;
+  sentMessage: string;
+  counterValue: number = 0;
+  counter: Observable<any>;
+  color = 'accent';
+  bft_enabled = false;
 
   constructor(websocketService: WebsocketService) {
     this.socket = websocketService.createWebsocket('wss://echo.websocket.org');
   }
 
   ngOnInit() {
-    this.socket.subscribe(message => this.message = message.data);
-  }
-
-  private launchCounter() {
-    // Counter already initialized
-    if (this.counterSubscription) {
-      this.counterSubscription.unsubscribe();
-    }
-
-    let counter = Observable.interval(1000);
-    this.counterSubscription = counter.subscribe(num => {
-      this.counter = num;
-      this.sentMessage = 'Websocket Message ' + this.counter;
-      this.socket.next(this.sentMessage);
+    this.socket.subscribe( (message) => {
+      this.message = message.data;
     });
   }
+
+  launchCounter() {
+    if(this.bft_enabled) {
+      // TODO call ServiceProxy invoke()
+    } else {
+      // Counter already initialized
+      if (this.counterSubscription) {
+        this.counterSubscription.unsubscribe();
+      }
+
+      this.counter = Observable.interval(1000);
+      this.counterSubscription = this.counter.subscribe((num) => {
+        this.counterValue++;
+        this.countChange.emit(this.counterValue);
+        this.sentMessage = 'Websocket Message ' + this.counterValue;
+        this.socket.next(this.sentMessage);
+      });
+    }
+  }
+
+  haltCounter() {
+    if(this.bft_enabled) {
+
+    } else {
+      if (this.counterSubscription) {
+        this.counterSubscription.unsubscribe();
+      }
+    }
+  }
+
+
+
 }
 
