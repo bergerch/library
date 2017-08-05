91f107a3867169e3f408180de914dda045fd442b
bergerch <bergerch@fim.uni-passau.de>
2017-08-05 17:14:38 +0200

    Refactoring WebSocket Handler and JSON Parsing

---
+++ src/bftsmart/communication/client/netty/TOMMessageJSON.java
@@ -0,0 +1,239 @@
+package bftsmart.communication.client.netty;
+
+import bftsmart.reconfiguration.views.View;
+import bftsmart.tom.core.messages.TOMMessage;
+import bftsmart.tom.core.messages.TOMMessageType;
+import bftsmart.tom.util.TOMUtil;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import javax.xml.bind.DatatypeConverter;
+import java.net.InetSocketAddress;
+import java.util.Map;
+
+
+/**
+ * This class handles the parsing and conversion of TOMMessage to JSON Objects / JSON String and vice versa
+ * Wrapper class for TOMMessage
+ */
+public class TOMMessageJSON {
+
+    /**
+     * JSON representation of TOMMessage
+     */
+    private JSONObject data;
+    private String hmac;
+
+    private TOMMessage tomMsg;
+
+    /**
+     * Do not use "data.toJSONString()"
+     */
+    private String dataString;
+
+    /**
+     * Parses the JSON String representation of TOMMessage and creates a TOMMessage object
+     *
+     * @param jsonMsg JSON String representation of TOMMessage
+     * @param useMacs if the JSON String representation of TOMMessage contains a hmac
+     */
+    public TOMMessageJSON(String jsonMsg, boolean useMacs) {
+
+        JSONParser parser = new JSONParser();
+
+        try {
+
+            // Parse message
+            Object obj = parser.parse(jsonMsg);
+            JSONObject jsonObject = (JSONObject) obj;
+            data = (JSONObject) jsonObject.get("data");
+
+            if (useMacs) {
+                hmac = (String) jsonObject.get("hmac");
+            }
+
+            // Initialize TOM message fields
+            int sender = ((Long) data.get("sender")).intValue();
+            int session = ((Long) data.get("session")).intValue();
+            int sequence = ((Long) data.get("sequence")).intValue();
+            int operationId = ((Long) data.get("operationId")).intValue();
+            int view = ((Long) data.get("viewId")).intValue();
+            TOMMessageType type = TOMMessageType.fromInt(((Long) data.get("type")).intValue());
+
+            // Parse content
+            byte[] content = data.get("content").toString().getBytes();
+
+            // Cuts off hmac and compute the data sequence of the received message
+            dataString = jsonMsg.replace("{\"data\":", "");
+            String[] words = dataString.split(",\"hmac");
+            dataString = words[0];
+
+            // Create the TOM Message
+            tomMsg = new TOMMessage(sender, session, sequence, operationId, content, view, type);
+            tomMsg.serializedMessage = TOMMessage.messageToBytes(tomMsg);
+
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    /**
+     * Initializes the JSON String representation from a TOMMessage object
+     *
+     * @param sm the TOMMessage
+     */
+    public TOMMessageJSON(TOMMessage sm) {
+
+        this.tomMsg = sm;
+
+        // Initialize TOM message fields
+        int sender = sm.getSender();
+        int session = sm.getSession();
+        int sequence = sm.getSequence();
+        int operationId = sm.getOperationId();
+        int view = sm.getViewID();
+        int type = sm.getReqType().toInt();
+        String content = "";
+
+        // IF it's a >>>Reconfiguration message<<< (clients view number was not up to date) create message with new VIEW
+        if (sm.view_change_response) {
+            System.out.println(">>>VIEW CHANGE RESPONSE<<<");
+
+            Object obj = TOMUtil.getObject(sm.getContent());
+            JSONObject viewJSON = new JSONObject();
+
+            // Set new view id and f for client to reconfigure to
+            viewJSON.put("id", new Integer(((View) obj).getId()));
+            viewJSON.put("f", new Integer(((View) obj).getF()));
+
+            // Set the processed array with proc ids
+            int[] processes = ((View) obj).getProcesses();
+            JSONArray processesJson = new JSONArray();
+            for (int k = 0; k < processes.length; k++) {
+                processesJson.add(new Integer(processes[k]));
+            }
+            viewJSON.put("processes", processesJson);
+
+            // Create the addresses array containing the replica set
+            Map<Integer, InetSocketAddress> map = ((View) obj).getAddresses();
+            JSONArray jsonArray = new JSONArray();
+            for (Map.Entry e : map.entrySet()) {
+
+                Integer i = (Integer) e.getKey();
+                InetSocketAddress addr = (InetSocketAddress) e.getValue();
+                String address = addr.getHostName();
+                Integer port = new Integer(addr.getPort());
+
+                JSONObject jsonValue = new JSONObject();
+                jsonValue.put("address", address);
+                jsonValue.put("port", port + 5);
+
+                JSONObject addEntry = new JSONObject();
+                addEntry.put("inetaddress", jsonValue);
+
+                jsonArray.add(addEntry);
+
+            }
+
+            viewJSON.put("addresses", jsonArray);
+
+            String jsonString = viewJSON.toJSONString();
+            content = content + jsonString;
+
+        } else {
+
+            byte[] reply = sm.getContent();
+            content = DatatypeConverter.printHexBinary(reply);
+        }
+
+        // Create JSON TOM Message
+        data = new JSONObject();
+        data.put("sender", new Integer(sender));
+        data.put("session", new Integer(session));
+        data.put("sequence", new Integer(sequence));
+        data.put("operationId", new Integer(operationId));
+        data.put("viewId", new Integer(view));
+        data.put("type", new Integer(type));
+        data.put("content", content);
+
+    }
+
+    /**
+     * Gets the data of a JSON TOM Message as JSON Object
+     *
+     * @return JSON Object data
+     */
+    public JSONObject getData() {
+        return this.data;
+    }
+
+    /**
+     * Attach a hmac to the JSON TOM Message
+     *
+     * @param hmac
+     * @return
+     */
+    public TOMMessageJSON attachHMAC(String hmac) {
+        // Create the JSON TOM Message
+        this.hmac = hmac;
+        return this;
+    }
+
+    /**
+     * Gets the a JSON TOM Message as JSON String
+     *
+     * @return JSON String message
+     */
+    public String getJSON() {
+        // Create the JSON TOM Message
+        JSONObject msg = new JSONObject();
+        msg.put("data", data);
+
+        if (this.hmac != null) {
+            msg.put("hmac", hmac);
+        }
+
+        return msg.toJSONString();
+    }
+
+    /**
+     * gets the TOMMessage object representation
+     *
+     * @return TOMMessage sm
+     */
+    public TOMMessage getTOMMsg() {
+        return this.tomMsg;
+    }
+
+    /**
+     * Gets the DataString of the JSON TOMMessage
+     * !!! IMPORTANT !!! Always use this method, and never "data.toJSONString()" as it is invariant and does not alter the
+     * permutation of the variables within the JSON String! "data.toJSONString()" is not a robust method and will lead
+     * to incorrect results when used for creating hmacs
+     *
+     * @return
+     */
+    public String getDataString() {
+        return this.dataString;
+    }
+
+    /**
+     * If the JSON String representation of the message has a hmac attached to it, it will be returned, else empty String
+     *
+     * @return
+     */
+    public String getHMAC() {
+        return this.hmac != null ? this.hmac : "";
+    }
+
+    @Override
+    public String toString() {
+        return this.getJSON();
+    }
+
+
+}
--- src/bftsmart/communication/client/netty/WebSocketHandler.java
+++ src/bftsmart/communication/client/netty/WebSocketHandler.java
@@ -2,10 +2,8 @@ package bftsmart.communication.client.netty;
 
 import bftsmart.communication.client.CommunicationSystemServerSide;
 import bftsmart.reconfiguration.util.TOMConfiguration;
-import bftsmart.reconfiguration.views.View;
 import bftsmart.tom.core.messages.TOMMessage;
-import bftsmart.tom.core.messages.TOMMessageType;
-import bftsmart.tom.util.TOMUtil;
+import bftsmart.tom.util.Logger;
 
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
@@ -16,19 +14,12 @@ import io.netty.handler.codec.http.websocketx.PongWebSocketFrame;
 import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
 import io.netty.handler.codec.http.websocketx.WebSocketFrame;
 
-import java.net.InetSocketAddress;
-
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.security.spec.InvalidKeySpecException;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Map;
-
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
 
 import javax.crypto.Mac;
 import javax.crypto.SecretKey;
@@ -40,89 +31,19 @@ import javax.xml.bind.DatatypeConverter;
 
 public class WebSocketHandler extends ChannelInboundHandlerAdapter {
 
-    private CommunicationSystemServerSide communicationSystemServer;
+    private NettyClientServerCommunicationSystemServerSide communicationServer;
     private TOMConfiguration config;
 
     public WebSocketHandler(CommunicationSystemServerSide communicationSystemServer) {
         super();
-        this.communicationSystemServer = communicationSystemServer;
-        ((NettyClientServerCommunicationSystemServerSide) communicationSystemServer).setWebSocketHandler(this);
-        this.config = ((NettyClientServerCommunicationSystemServerSide) communicationSystemServer).getController().getStaticConf();
+        this.communicationServer = (NettyClientServerCommunicationSystemServerSide) communicationSystemServer;
+        communicationServer.setWebSocketHandler(this);
+        this.config = communicationServer.getController().getStaticConf();
     }
-
 
     public void send(ArrayList<WebClientServerSession> webClientReceivers, TOMMessage sm) {
 
-        // Initialize TOM message fields
-        int sender = sm.getSender();
-        int session = sm.getSession();
-        int sequence = sm.getSequence();
-        int operationId = sm.getOperationId();
-        int view = sm.getViewID();
-        int type = sm.getReqType().toInt();
-        String content = "";
-
-        // IF it's a >>>Reconfiguration message<<< (clients view number was not up to date) create message with new VIEW
-        if (sm.view_change_response) {
-            System.out.println(">>>VIEW CHANGE RESPONSE<<<");
-
-            Object obj = TOMUtil.getObject(sm.getContent());
-            JSONObject viewJSON = new JSONObject();
-
-            // Set new view id and f for client to reconfigure to
-            viewJSON.put("id", new Integer(((View) obj).getId()));
-            viewJSON.put("f", new Integer(((View) obj).getF()));
-
-            // Set the processed array with proc ids
-            int[] processes = ((View) obj).getProcesses();
-            JSONArray processesJson = new JSONArray();
-            for (int k = 0; k < processes.length; k++) {
-                processesJson.add(new Integer(processes[k]));
-            }
-            viewJSON.put("processes", processesJson);
-
-            // Create the addresses array containing the replica set
-            Map<Integer, InetSocketAddress> map = ((View) obj).getAddresses();
-            JSONArray jsonArray = new JSONArray();
-            for (Map.Entry e : map.entrySet()) {
-
-                Integer i = (Integer) e.getKey();
-                InetSocketAddress addr = (InetSocketAddress) e.getValue();
-                String address = addr.getHostName();
-                Integer port = new Integer(addr.getPort());
-
-                JSONObject jsonValue = new JSONObject();
-                jsonValue.put("address", address);
-                jsonValue.put("port", port + 5);
-
-                JSONObject addEntry = new JSONObject();
-                addEntry.put("inetaddress", jsonValue);
-
-                jsonArray.add(addEntry);
-
-            }
-
-            viewJSON.put("addresses", jsonArray);
-
-            String jsonString = viewJSON.toJSONString();
-            content = content + jsonString;
-
-        } else {
-
-            byte[] reply = sm.getContent();
-            content = DatatypeConverter.printHexBinary(reply);
-        }
-
-        // Create JSON TOM Message
-        JSONObject data = new JSONObject();
-        data.put("sender", new Integer(sender));
-        data.put("session", new Integer(session));
-        data.put("sequence", new Integer(sequence));
-        data.put("operationId", new Integer(operationId));
-        data.put("viewId", new Integer(view));
-        data.put("type", new Integer(type));
-        data.put("content", content);
-
+        TOMMessageJSON tomMessageJSON = new TOMMessageJSON(sm);
 
         // For each client receiver, attach mac or signature (if used), then send message
         for (WebClientServerSession clientSession : webClientReceivers) {
@@ -143,14 +64,13 @@ public class WebSocketHandler extends ChannelInboundHandlerAdapter {
                         String str = clientSession.getClientId() + ":" + config.getProcessId();
                         PBEKeySpec spec = new PBEKeySpec(str.toCharArray());
                         SecretKey authKey = fac.generateSecret(spec);
-
                         macSend = Mac.getInstance(config.getHmacAlgorithm());
                         macSend.init(authKey);
                         clientSession.setMacSend(macSend);
                     }
 
                     // Generate the HMAC for the data to be transmitted
-                    String dataString = data.toJSONString();
+                    String dataString = tomMessageJSON.getData().toJSONString();
                     byte[] hmacComputedBytes = macSend.doFinal(dataString.getBytes());
                     String hmacHexString = DatatypeConverter.printHexBinary(hmacComputedBytes);
                     hmac = hmacHexString.toLowerCase();
@@ -162,22 +82,12 @@ public class WebSocketHandler extends ChannelInboundHandlerAdapter {
                 } catch (InvalidKeyException e) {
                     e.printStackTrace();
                 }
-
             }
 
-            // Create the JSON TOM Message and send it to the client
-            JSONObject msg = new JSONObject();
-            msg.put("data", data);
-
-            if (useMacs) {
-                msg.put("hmac", hmac);
-            }
-
-            String jsonMsg = msg.toJSONString();
-
-            System.out.println(" Client <--- Replica | TextWebSocketFrame Sent : " + jsonMsg);
-
+            //  Create the JSON TOM Message and send it to the client. if Mac are used, attach it
+            String jsonMsg = useMacs ? tomMessageJSON.attachHMAC(hmac).getJSON() : tomMessageJSON.getJSON();
             clientSession.getCtx().writeAndFlush(new TextWebSocketFrame(jsonMsg));
+            Logger.println(" Client <--- Replica | TextWebSocketFrame Sent : " + jsonMsg);
         }
 
     }
@@ -193,56 +103,29 @@ public class WebSocketHandler extends ChannelInboundHandlerAdapter {
                 //  System.out.println(((BinaryWebSocketFrame) msg).content());
             } else if (msg instanceof TextWebSocketFrame) {
 
-                String jsonMsg = ((TextWebSocketFrame) msg).text();
+                Logger.println(" Client ---> Replica | TextWebSocketFrame Received : " + ((TextWebSocketFrame) msg).text());
 
-                System.out.println(" Client ---> Replica | TextWebSocketFrame Received : " + jsonMsg);
-
-                JSONParser parser = new JSONParser();
+                boolean useMacs = config.getUseMACs() == 1;
+                TOMMessageJSON tomMessageJSON = new TOMMessageJSON(((TextWebSocketFrame) msg).text(), useMacs);
 
                 try {
 
-                    boolean useMacs = config.getUseMACs() == 1;
-
-                    // Parse message
-                    Object obj = parser.parse(jsonMsg);
-                    JSONObject jsonObject = (JSONObject) obj;
-                    JSONObject data = (JSONObject) jsonObject.get("data");
-
-                    String hmacReceived = "";
-                    if (useMacs) {
-                        hmacReceived = (String) jsonObject.get("hmac");
-                    }
-
-                    int sender = ((Long) data.get("sender")).intValue();
-
-                    WebClientServerSession wcss = new WebClientServerSession(ctx,
-                            ((NettyClientServerCommunicationSystemServerSide) communicationSystemServer).getController().
-                                    getStaticConf().getProcessId(), sender);
-
-                    HashMap<Integer, WebClientServerSession> webClients =
-                            ((NettyClientServerCommunicationSystemServerSide) communicationSystemServer).getWebClientConnections();
+                    // Add websocket connection to map of all web clients
+                    int sender = tomMessageJSON.getTOMMsg().getSender();
+                    WebClientServerSession wcss = new WebClientServerSession(ctx, communicationServer.getController()
+                            .getStaticConf().getProcessId(), sender);
+                    HashMap<Integer, WebClientServerSession> webClients = communicationServer.getWebClientConnections();
 
                     if (!webClients.containsValue(wcss)) {
                         webClients.put(new Integer(sender), wcss);
                     }
-
-                    // Initialize TOM message fields
-                    int session = ((Long) data.get("session")).intValue();
-                    int sequence = ((Long) data.get("sequence")).intValue();
-                    int operationId = ((Long) data.get("operationId")).intValue();
-                    int view = ((Long) data.get("viewId")).intValue();
-                    TOMMessageType type = TOMMessageType.fromInt(((Long) data.get("type")).intValue());
-                    byte[] content = new byte[1];
-
-                    // Parse content
-                    content = data.get("content").toString().getBytes();
 
                     // Compute HMAC and Compare
                     if (useMacs) {
 
                         // Performance Optimization (Save Mac object in Map) and reuse
                         HashMap<Integer, WebClientServerSession> connections =
-                                ((NettyClientServerCommunicationSystemServerSide) communicationSystemServer).getWebClientConnections();
+                                communicationServer.getWebClientConnections();
                         WebClientServerSession webConnection = connections.get(new Integer(sender));
                         Mac macReceive = webConnection.getMacReceived();
 
@@ -255,15 +138,12 @@ public class WebSocketHandler extends ChannelInboundHandlerAdapter {
                             macReceive = Mac.getInstance(config.getHmacAlgorithm());
                             macReceive.init(authKey);
                             webConnection.setMacReceived(macReceive);
-
                         }
 
-                        // Cuts off hmac and compute the data sequence of the received message
-                        String dataString = jsonMsg.replace("{\"data\":", "");
-                        String[] words = dataString.split(",\"hmac");
-                        dataString = words[0];
+                        String hmacReceived = tomMessageJSON.getHMAC();
 
                         // Compute the hmac of this received data
+                        String dataString = tomMessageJSON.getDataString();
                         byte[] hmacComputedBytes = macReceive.doFinal(dataString.getBytes());
                         String hmacComputed = DatatypeConverter.printHexBinary(hmacComputedBytes);
                         hmacComputed = hmacComputed.toLowerCase();
@@ -282,20 +162,13 @@ public class WebSocketHandler extends ChannelInboundHandlerAdapter {
                     }
 
                     // Create the TOM Message and deliver it to TOM LAYER
-                    TOMMessage sm = new TOMMessage(sender, session, sequence, operationId, content, view, type);
-                    sm.serializedMessage = TOMMessage.messageToBytes(sm);
-
-                    if (((NettyClientServerCommunicationSystemServerSide) communicationSystemServer)
-                            .getRequestReceiver() != null) {
-                        ((NettyClientServerCommunicationSystemServerSide) communicationSystemServer)
-                                .getRequestReceiver().requestReceived(sm);
+                    if (communicationServer.getRequestReceiver() != null) {
+                        communicationServer.getRequestReceiver().requestReceived(tomMessageJSON.getTOMMsg());
                     }
-
 
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
-
 
             } else if (msg instanceof PingWebSocketFrame) {
                 System.out.println("PingWebSocketFrame Received : ");
