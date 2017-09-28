a1940d76c8d694c60d6bbc26ab3ca1d9a9f9d744
bergerch <bergerch@fim.uni-passau.de>
2017-09-28 18:06:45 +0200

    working on benchmark

--- src/web-bft/src/app/Microbenchmarks/throughputLatency.component.ts
+++ src/web-bft/src/app/Microbenchmarks/throughputLatency.component.ts
@@ -1,7 +1,6 @@
 import {Component, Input, Output, EventEmitter, OnInit} from '@angular/core';
-import {ServiceProxy} from "../../bft/tom/ServiceProxy.service";
-import {TOMConfiguration} from "../../bft/config/TOMConfiguration";
-import { WebWorkerService } from '../../../node_modules/angular2-web-worker/web-worker';
+import {Observable} from "rxjs/Observable";
+import {Subscription} from "rxjs/Subscription";
 
 @Component({
   selector: 'throughputLatency',
@@ -11,10 +10,10 @@ import { WebWorkerService } from '../../../node_modules/angular2-web-worker/web-
 export class ThroughputLatency implements OnInit{
 
   processId: number = 1000; // Id of the first process
-  threadNumber: number = 20; // Number of Threads used in the system
+  threadNumber: number = 4; // Number of Threads used in the system
   numberOfOps : number = 10000; // How many operations each client executs e.g. the number of requests
   requestSize : number = 0; // Number of Bytes of content field for a message
-  interval : number = 50; // Milliseconds a client waits before sending the next request
+  interval : number = 5; // Milliseconds a client waits before sending the next request
   readOnly : boolean = false; // If client should send read-only requests instead of ordered requests
   verbose : boolean = true; // Enable for additional output;
   dos : boolean = false; // For simulating a dos attack
@@ -22,6 +21,9 @@ export class ThroughputLatency implements OnInit{
   progress: number = 0; // For progress bar
   output;
   started: boolean = false;
+
+  progressObservable: Observable<any>;
+  progressSubscription: Subscription;
 
   constructor() {
 
@@ -31,6 +33,8 @@ export class ThroughputLatency implements OnInit{
 
    this.output = document.getElementById('output');
    this.started = false;
+
+
 
   }
 
@@ -51,13 +55,35 @@ export class ThroughputLatency implements OnInit{
 
   startBenchmark() {
     this.started = true;
-    this.println('Benchmark started with ' + this.threadNumber + ' threads. ' + 'Each thread will send ' + this.numberOfOps
+    if (this.verbose) this.println('Benchmark started with ' + this.threadNumber + ' threads. ' + 'Each thread will send ' + this.numberOfOps
     + ' requests. The request content size is set to ' + this.requestSize + ' Bytes. Each thread will wait at least ' + this.interval
       + ' ms before sending the next request. Read-only: ' + this.readOnly + ' Verbose: ' + this.verbose + ' dos: ' + this.dos +
     ' Note the browser will send up to ' + this.threadNumber*1000/this.interval + ' requests per second.' );
 
 
+    let runner = document.getElementById("runner");
+
+    for (let i = 0; i < this.threadNumber; i++) {
+      runner.click();
+    }
+
+
+    let i = 100;
+    let n = 0;
+
+    this.progressObservable = Observable.interval(i);
+    this.progressSubscription = this.progressObservable.subscribe((num) => {
+
+      n+= (i/this.interval);
+
+      this.progress = n / this.numberOfOps * 100;
+
+
+    });
+
+
   }
+
 
 
   download() {
