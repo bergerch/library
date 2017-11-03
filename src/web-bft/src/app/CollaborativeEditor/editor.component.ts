19a25cb5d88ba200c9a7d1584ed44f4e1dadb8a1
bergerch <bergerch@fim.uni-passau.de>
2017-11-03 16:57:51 +0100

    Wokring on Editor

--- src/web-bft/src/app/CollaborativeEditor/editor.component.ts
+++ src/web-bft/src/app/CollaborativeEditor/editor.component.ts
@@ -200,5 +200,48 @@ export class Editor implements OnInit, ReplyListener {
 
     return charCount;
   };
+
+
+  /**
+   * Test purpose
+   */
+  autoWrite() {
+
+
+    let shakespeare = 'FROM fairest creatures we desire increase,\n' +
+      'That thereby beauty\'s rose might never die,\n' +
+      'But as the riper should by time decease,\n' +
+      'His tender heir might bear his memory:\n' +
+      'But thou, contracted to thine own bright eyes,\n' +
+      'Feed\'st thy light\'st flame with self-substantial fuel,\n' +
+      'Making a famine where abundance lies,\n' +
+      'Thyself thy foe, to thy sweet self too cruel.\n' +
+      'Thou that art now the world\'s fresh ornament\n' +
+      'And only herald to the gaudy spring,\n' +
+      'Within thine own bud buriest thy content\n' +
+      'And, tender churl, makest waste in niggarding.\n' +
+      'Pity the world, or else this glutton be,\n' +
+      'To eat the world\'s due, by the grave and thee.';
+
+    let observable = Observable.interval(150);
+    let subscription = observable.subscribe((num) => {
+
+      // part of Differential Synchronisation: create a Diff
+      let write = this.editor.innerHTML;
+
+      write = write + shakespeare.charAt(num);
+
+      let d = this.dmp.diff_main(this.client_shadow, write);
+      this.dmp.diff_cleanupSemantic(d);
+      this.client_shadow = write;
+
+      // Send write command to replica set
+      this.editorProxy.invokeOrdered({operation: 'write', data: d}, this);
+
+    });
+
+  }
+
+
 }
 
